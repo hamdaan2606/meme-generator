a76e75a10fffc8eff27b97b26632768afe17410e
hamdaan2606 <130116779+hamdaan2606@users.noreply.github.com>
2026-03-19 16:30:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const btn = document.getElementById('btn');
-const img = document.getElementById('meme');
-const loading = document.getElementById('loading');
-const title = document.getElementById('title');
-
 btn.addEventListener('click', () => {
 
+    btn.disabled = true;
     loading.innerText = "Loading...";
     img.src = "";
     title.innerText = "";
 
     fetch('https://meme-api.com/gimme')
-        .then(res => res.json())
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("API error");
+            }
+            return res.json();
+        })
         .then(data => {
             img.src = data.url;
+            img.alt = data.title;
             title.innerText = data.title;
             loading.innerText = "";
+            btn.disabled = false;
         })
         .catch(() => {
-            loading.innerText = "Failed to load meme. Try again.";
+            loading.innerText = "Something went wrong. Try again.";
+            btn.disabled = false;
         });
 
 });
